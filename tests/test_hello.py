cd409f091260305d86d4eaff9ac85645d9860080
Diego Restrepo <restrepo@udea.edu.co>
2022-11-29 17:40:18 -0500

    Update test_hello.py

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,11 +1,11 @@
 import unittest
-from desoper import hello
+from yukawa import yukawa
 
 
 class Test_hello(unittest.TestCase):
     def test__working(self):
-        self.assertEqual(hello.hello(),
-                         'Hello, World!', True)
+        self.assertEqual(yukawa.get_massive_fermios([1, 3, 4, 6, -7, -8, -13, 14],7).get('s'),
+                         7, True)
 
 
 if __name__ == '__main__':
