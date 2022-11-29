6291ddc1cb279b9a0e68bf2a8c0c9f47bdd08f70
Diego Restrepo <restrepo@udea.edu.co>
2022-11-29 18:41:17 -0500

    lc

--- setup.py
+++ setup.py
@@ -34,10 +34,10 @@ if os.name in ('nt', 'dos'):
 def main():
     setup(
         # Application name:
-        name="desoper",
+        name="yukawa",
 
         # Version number (initial):
-        version="0.0.3",
+        version="0.0.1",
 
         # Application author details:
         author="restrepo",
@@ -50,11 +50,11 @@ def main():
         include_package_data=True,
 
         # Details
-        url="https://github.com/restrepo/DevOps",
+        url="https://github.com/restrepo/yukawa",
 
         license="BSD",
 
-        description="Hello World!",
+        description="Get massive fermions from a given scalar Abelian charge",
 
         long_description=open("README.md").read(),
 
